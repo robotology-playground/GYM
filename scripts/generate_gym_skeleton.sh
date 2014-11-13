cf6a152c611968d47d06d738edeaab0aab279edc
Luca Muratore <luca.muratore@iit.it>
2014-11-13 15:24:02 +0100

    added the download of the skeleton

--- scripts/generate_gym_skeleton.sh
+++ scripts/generate_gym_skeleton.sh
@@ -5,7 +5,13 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+# SVN hack thanks to gituhb - TODO: change this
+svn export https://github.com/robotology-playground/GYM/trunk/skeleton/ > /dev/null
+mv skeleton/* . && rm -rf skeleton
 
-
+# find and replace 
 find . -maxdepth 2 -type f -not -path '*/\.*' -exec sed -i -e "s/_MODULE_PREFIX_/$1/g" {} \;
+# rename
 find . -maxdepth 2 -type f -not -path '*/\.*' -not -name "CMakeLists.txt" -not -name "README.md" -exec bash -c 'dir=$(dirname $0) && file=$(basename $0) && mv $0 "$dir/$1_$file"' {} $1 \;
+
+echo "GYM skeleton is ready. Have fun!"
