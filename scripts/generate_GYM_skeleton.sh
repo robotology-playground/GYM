3a26de293b560ff3a732318442ead55ac561f5a4
Luca Muratore <luca.muratore@iit.it>
2014-11-13 16:04:10 +0100

    Added the install for generate_GYM_skeleton script and modify the find maxdepth to 3

--- scripts/generate_GYM_skeleton.sh
+++ scripts/generate_GYM_skeleton.sh
@@ -10,8 +10,8 @@ svn export https://github.com/robotology-playground/GYM/trunk/skeleton/ > /dev/n
 mv skeleton/* . && rm -rf skeleton
 
 # find and replace 
-find . -maxdepth 2 -type f -not -path '*/\.*' -exec sed -i -e "s/_MODULE_PREFIX_/$1/g" {} \;
+find . -maxdepth 3 -type f -not -path '*/\.*' -exec sed -i -e "s/_MODULE_PREFIX_/$1/g" {} \;
 # rename
-find . -maxdepth 2 -type f -not -path '*/\.*' -not -name "CMakeLists.txt" -not -name "README.md" -exec bash -c 'dir=$(dirname $0) && file=$(basename $0) && mv $0 "$dir/$1_$file"' {} $1 \;
+find . -maxdepth 3 -type f -not -path '*/\.*' -not -name "CMakeLists.txt" -not -name "README.md" -exec bash -c 'dir=$(dirname $0) && file=$(basename $0) && mv $0 "$dir/$1_$file"' {} $1 \;
 
 echo "GYM skeleton is ready. Have fun!"
